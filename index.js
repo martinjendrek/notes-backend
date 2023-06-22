9df5c4dacaceb1d648945cdb78cc0c9cc6384844
Martin Jendrek <martin.jendrek1@gmail.com>
2023-06-22 21:19:59 +0200

    port modify

--- index.js
+++ index.js
@@ -92,6 +92,7 @@ app.post('/api/notes', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
