02259179dcebbaeb3833d79062bc24f5aab1919e
Martin Jendrek <martin.jendrek1@gmail.com>
2023-06-23 09:14:07 +0200

    Add frontend part

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
-const cors = require('cors'
-)
+const cors = require('cors')
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path:  ', request.path)
@@ -14,7 +13,9 @@ const requestLogger = (request, response, next) => {
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
+
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 app.use(requestLogger)
 
